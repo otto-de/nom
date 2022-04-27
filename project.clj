f1f7b04c2bc61d7153e82b35c6fa35e4de7efb89
Svante von Erichsen <svante.v.erichsen@web.de>
2022-04-27 15:37:50 +0200

    lein: mark clojure as provided

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/otto-de/nom"
   :license {:name "Apache License 2.0"
             :url  "http://www.apache.org/license/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]]
   :source-paths ["src/cljc"]
   :test-paths ["test/cljc"]
   :cljfmt {:indents {let-nom [[:inner 0]]
